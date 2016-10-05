c0f619c587432582645bfa261ed6549e7b930682
Élysson MR <elyssonmr@gmail.com>
2016-10-05 00:17:32 -0300

    Refatorações em alguns nomes

--- test_editor_grafico.py
+++ test_editor_grafico.py
@@ -4,34 +4,34 @@ from editor_grafico import parsear_comando
 
 
 class PasearComandoTestCase(unittest.TestCase):
-    def test_parse_comando_criar_matriz(self):
-        comando_criar = "I 10 10"
+    def test_parse_cmd_criar_matriz(self):
+        cmd_criar = "I 10 10"
         esperado = {'comando': 'criar', 'args': ['10', '10']}
-        resp = parsear_comando(comando_criar)
+        resp = parsear_comando(cmd_criar)
 
         self.assertEqual(esperado, resp)
 
-    def test_parse_comando_limpa_matriz(self):
-        comando_limpar = "C"
+    def test_parse_cmd_limpar_matriz(self):
+        cmd_limpar = "C"
         esperado = {'comando': 'limpar', 'args': []}
-        resp = parsear_comando(comando_limpar)
+        resp = parsear_comando(cmd_limpar)
 
         self.assertEqual(esperado, resp)
 
-    def test_parse_comando_colorir_pixel(self):
-        comando_colorir = "L 10 1 C"
+    def test_parse_cmd_colorir_pixel(self):
+        cmd_colorir = "L 10 1 C"
         esperado = {'comando': "colorir", 'args': ['10', '1', 'C']}
-        resp = parsear_comando(comando_colorir)
+        resp = parsear_comando(cmd_colorir)
 
         self.assertEqual(esperado, resp)
 
-    def test_parse_comando_desenhar_seg_vertical(self):
-        comando_desenhar_segmento = "V 10 1 2 C"
+    def test_parse_cmd_desenhar_seg_vertical(self):
+        cmd_desenhar_segmento = "V 10 1 2 C"
         esperado = {
             'comando': 'desenhar_seg_vertical',
             'args': ['10', '1', '2', 'C']
         }
-        resp = parsear_comando(comando_desenhar_segmento)
+        resp = parsear_comando(cmd_desenhar_segmento)
 
         self.assertEqual(esperado, resp)
 
